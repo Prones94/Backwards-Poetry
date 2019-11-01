0fae7d128b6d9a5a43d3430933a7451e214cd297
Ian Rones <ronesp.85@gmail.com>
2019-11-01 08:48:12 -0700

    [add] lines_printed_backwards function works

---
+++ app.py
@@ -0,0 +1,17 @@
+# user_poem = input('Please enter your favorite poem. Poem should include original fomatting')
+poem = """If you are a dreamer, come in,
+If you are a dreamer, a wisher, a liar,
+A hope-er, a pray-er, a magic bean buyer...
+If you're a pretender, come sit by my fire.
+For we have some flax-golden tales to spin.
+Come in!
+Come in!"""
+
+def lines_printed_backwards(poem):
+    split_poem = (poem.split("\n"))
+    len_poem = len(split_poem)-1
+    for _ in split_poem:
+        print(split_poem[len_poem])
+        len_poem -= 1
+
+lines_printed_backwards(poem)
