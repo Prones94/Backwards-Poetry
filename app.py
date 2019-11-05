f5ca19747895dfd61866d945ec6c3fe31a3a4fe4
Ian Rones <ronesp.85@gmail.com>
2019-11-05 00:12:07 -0800

    [fix] lines_printed_backward function prints btoh the index number and the reversed string

--- app.py
+++ app.py
@@ -6,19 +6,23 @@ If you're a pretender, come sit by my fire.
 For we have some flax-golden tales to spin.
 Come in!
 Come in!"""
+split = poem.split("\n")
 
-def lines_printed_backwards(poem):
-    split_poem = (poem.split("\n"))
-    len_poem = len(split_poem)-1
-    for _ in split_poem:
-        print(split_poem[len_poem])
+def lines_printed_backwards(split):
+    split.reverse()
+    len_poem = len(split)
+    for i in range(len(split)):
+        phrase = split[i]
+        print(f'{len_poem}, {phrase}')
         len_poem -= 1
+    
 
 def print_index_number(poem):
     split_poem = (poem.split("\n"))
-    for line_num in split_poem:
-        print(split_poem.index(line_num))
+    for i, line_num in enumerate(split_poem, 1):
+        print(i, split_poem)
 
 # def print_random
-lines_printed_backwards(poem)
-print_index_number(poem)
+# print(split_poem(poem))
+lines_printed_backwards(split)
+# print_index_number(poem)
